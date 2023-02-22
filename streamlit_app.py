69df1685f66de2a6f2c18db788862f7f0f64f9d6
matiasgarroa <matiasgarroalou@gmail.com>
2023-02-22 10:18:52 -0300

    prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
-#streamlit_app.py
+import streamlit as st
+from google.cloud import bigquery
+
+# Carga tus credenciales desde el archivo JSON descargado
+credentials_path = 'pi-soy-henry-67f5715648a9.json'
+client = bigquery.Client.from_service_account_json(credentials_path)
+
+# Ejemplo de consulta SQL
+query = '''
+SELECT *
+FROM `hechos`
+LIMIT 10
+'''
+
+# Ejecuta la consulta y almacena los resultados en un dataframe de Pandas
+df = client.query(query).to_dataframe()
+
+# Muestra los resultados en Streamlit
+st.write(df)

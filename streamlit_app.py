795d6cb09103fa70635e128d43b5093269494d03
matiasgarroa <matiasgarroalou@gmail.com>
2023-02-23 12:56:06 -0300

    prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 
+# Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query(query):
@@ -19,29 +20,27 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['word'])
+
+# Ejemplo de consulta SQL
+rows = run_query("SELECT * FROM `pi-soy-henry.migrations.indicadores` LIMIT 10")
+
+for row in rows:
+    st.write(row['indicator_name'])
+
 sql_indicadores = """
 SELECT * FROM `pi-soy-henry.migrations.indicadores`
 """
 indicadores = pandas_gbq.read_gbq(sql_indicadores, project_id='pi-soy-henry')
 
+st.write(indicadores)
+
 sql_hechos = """
 SELECT * FROM `pi-soy-henry.migrations.hechos`
 """
 hechos = pandas_gbq.read_gbq(sql_hechos, project_id='pi-soy-henry')
-
-####################
-### INTRODUCCIÓN ###
-####################
-
-row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
-with row0_1:
-    st.title('Grupo10 - Analizador de Migraciones')
-with row0_2:
-    st.text("")
-    st.subheader('Streamlit App')
-row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
-with row3_1:
-    st.markdown("Hola! La migración es un fenómeno global que afecta a muchas personas en todo el mundo. Hay muchas razones por las que la gente decide emigrar, incluyendo motivos económicos, políticos, sociales y de seguridad. Algunos de los desafíos más comunes que enfrentan los migrantes incluyen la discriminación, la falta de recursos, apoyo, y la separación de sus seres queridos. ")
-    st.markdown("Queremos proporcionarte una herramienta útil y accesible, para ayudarte tomar decisiones informadas sobre tu destino de emigración.")
-    st.markdown("Podrás encontrar el codigo fuente en [PF Henry GitHub Repository](https://github.com/matiasgarroa/Grupo10-Proyecto-Final-Migraciones)")
-

15d478d4fae177015d42bf38323b37eac6e264a2
matiasgarroa <matiasgarroalou@gmail.com>
2023-02-23 11:00:02 -0300

    prueba

---
+++ streamlit_app copy.py
@@ -0,0 +1,46 @@
+#import streamlit as st
+#from google.oauth2 import service_account
+#from google.cloud import bigquery
+#import pandas as pd
+#import pandas_gbq
+#
+## Create API client.
+#credentials = service_account.Credentials.from_service_account_info(
+#    st.secrets["gcp_service_account"]
+#)
+#client = bigquery.Client(credentials=credentials)
+#
+## Perform query.
+## Uses st.cache_data to only rerun when the query changes or after 10 min.
+#@st.cache_data(ttl=600)
+#def run_query(query):
+#    query_job = client.query(query)
+#    rows_raw = query_job.result()
+#    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+#    rows = [dict(row) for row in rows_raw]
+#    return rows
+#
+#rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+#
+## Print results.
+#st.write("Some wise words from Shakespeare:")
+#for row in rows:
+#    st.write("✍️ " + row['word'])
+#
+## Ejemplo de consulta SQL
+#rows = run_query("SELECT * FROM `pi-soy-henry.migrations.indicadores` LIMIT 10")
+#
+#for row in rows:
+#    st.write(row['indicator_name'])
+#
+#sql_indicadores = """
+#SELECT * FROM `pi-soy-henry.migrations.indicadores`
+#"""
+#indicadores = pandas_gbq.read_gbq(sql_indicadores, project_id='pi-soy-henry')
+#
+#st.write(indicadores)
+#
+#sql_hechos = """
+#SELECT * FROM `pi-soy-henry.migrations.hechos`
+#"""
+#hechos = pandas_gbq.read_gbq(sql_hechos, project_id='pi-soy-henry')
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,37 +10,27 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
-
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write("✍️ " + row['word'])
-
-# Ejemplo de consulta SQL
-rows = run_query("SELECT * FROM `pi-soy-henry.migrations.indicadores` LIMIT 10")
-
-for row in rows:
-    st.write(row['indicator_name'])
-
 sql_indicadores = """
 SELECT * FROM `pi-soy-henry.migrations.indicadores`
 """
 indicadores = pandas_gbq.read_gbq(sql_indicadores, project_id='pi-soy-henry')
 
-st.write(indicadores)
-
 sql_hechos = """
 SELECT * FROM `pi-soy-henry.migrations.hechos`
 """
 hechos = pandas_gbq.read_gbq(sql_hechos, project_id='pi-soy-henry')
+
+# Define los widgets de la sección izquierda
+st.sidebar.title("Sección izquierda")
+name = st.sidebar.text_input("Ingresa tu nombre")
+age = st.sidebar.number_input("Ingresa tu edad")
+
+# Define los widgets de la sección derecha
+st.title("Sección derecha")
+color = st.selectbox("¿Cuál es tu color favorito?", ["Rojo", "Azul", "Verde"])
+animal = st.selectbox("¿Cuál es tu animal favorito?", ["Perro", "Gato", "Pájaro"])
+
+# Muestra los resultados
+st.write("Hola, " + name + "!")
+st.write("Tienes " + str(age) + " años.")
+st.write("Tu color favorito es " + color + " y tu animal favorito es el " + animal + ".")

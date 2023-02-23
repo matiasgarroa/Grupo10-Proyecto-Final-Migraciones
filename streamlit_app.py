b9a8326465f34e8516ac755a34684ec68b1161e1
matiasgarroa <matiasgarroalou@gmail.com>
2023-02-23 12:24:37 -0300

    pruwba

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,15 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
 sql_indicadores = """
 SELECT * FROM `pi-soy-henry.migrations.indicadores`
 """
